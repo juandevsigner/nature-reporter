5c6ef3c0b5aba07fc54664f71da00806cb281e3a
juandevsigner <juanda.core07@gmail.com>
2025-03-13 17:58:09 -0500

    Se agrego imagenes a nido

--- src/schemas/termNature/nidos.schema.ts
+++ src/schemas/termNature/nidos.schema.ts
@@ -1,11 +1,40 @@
 import { ITerm } from "src/interface/schemas.interface";
 
 export const NEST: ITerm = {
-  phrase: "Aqui el nido",
+  phrase:
+    "Para el hombre, como para el pájaro, el mundo ofrece muchos sitios donde posarse, pero nidos solamente uno: Su Hogar",
   list: [
     {
-      url: "https://res.cloudinary.com/dbgww54tl/image/upload/v1711843437/photo1708723242_buwjys.jpg",
-      description: "Ilustración de Isabel Alía",
+      url: "https://res.cloudinary.com/dbgww54tl/image/upload/v1741899753/Imagen_de_WhatsApp_2025-02-14_a_las_11.40.24_86c9d10f_zmma19.jpg",
+      description: "",
+    },
+    {
+      url: "https://res.cloudinary.com/dbgww54tl/image/upload/v1741899753/Imagen_de_WhatsApp_2025-02-20_a_las_14.40.32_01a6039b_bvxznz.jpg",
+      description: "",
+    },
+    {
+      url: "https://res.cloudinary.com/dbgww54tl/image/upload/v1741899744/Imagen_de_WhatsApp_2025-02-21_a_las_17.17.23_d158ab42_ejkse7.jpg",
+      description: "",
+    },
+    {
+      url: "https://res.cloudinary.com/dbgww54tl/image/upload/v1741899744/Imagen_de_WhatsApp_2025-02-21_a_las_19.58.07_402ff320_jitxks.jpg",
+      description: "",
+    },
+    {
+      url: "https://res.cloudinary.com/dbgww54tl/image/upload/v1741899744/Imagen_de_WhatsApp_2025-02-28_a_las_08.11.43_232722e0_unl3iv.jpg",
+      description: "",
+    },
+    {
+      url: "https://res.cloudinary.com/dbgww54tl/image/upload/v1741899743/Imagen_de_WhatsApp_2025-02-28_a_las_09.59.00_7e199005_ouaunz.jpg",
+      description: "",
+    },
+    {
+      url: "https://res.cloudinary.com/dbgww54tl/image/upload/v1741899743/Imagen_de_WhatsApp_2025-02-28_a_las_10.19.57_714b0ee6_qnzihd.jpg",
+      description: "",
+    },
+    {
+      url: "https://res.cloudinary.com/dbgww54tl/image/upload/v1741899743/Imagen_de_WhatsApp_2025-03-10_a_las_20.13.51_793e3c35_vxmgdn.jpg",
+      description: "",
     },
   ],
 };
